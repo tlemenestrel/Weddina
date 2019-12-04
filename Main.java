a3866a77666b8896f718ef32f56297de78f751ee
Thomas Le Menestrel <34300678+tlemenestrel@users.noreply.github.com>
2019-12-04 18:06:32 +0000

    Add files via upload

--- Main.java
+++ Main.java
@@ -35,13 +35,14 @@ public class Main {
 		Guests.guestsResponses.add("Coming");
 		Guests.seatNumber.add(1);
 
-		// Guests.guestsResponses.ensureCapacity(50);
+		Businesses.venueEmails.add("tlm@");
+		Businesses.venuePasswords.add("ABC");
 
-		//for (Boolean response: Guests.guestsResponses) {
+		Businesses.bandEmails.add("@tlm@");
+		Businesses.bandPasswords.add("ACB");
 
-		//	response = false;
-
-		// }
+		Businesses.catererEmails.add("@tlm");
+		Businesses.catererPasswords.add("BAC");
 
 	}
 
@@ -54,9 +55,9 @@ public class Main {
 
 		Scanner scanner = new Scanner(System.in);
 
-		messageForChoosingTypeOfUser();
-
 		while (condition1) {
+
+			messageForChoosingTypeOfUser();
 
 			char input = scanner.next().charAt(0); 
 			scanner.nextLine();
@@ -72,6 +73,51 @@ public class Main {
 				Guests.handlingGuests();
 				condition1 = false;
 
+			}
+
+			else if (input == 'b') {
+
+				boolean condition2 = true;
+
+				while (condition2) {
+
+					System.out.println("");
+					System.out.println("Press c for caterer, v for venue or b for band:");
+
+					char input2 = scanner.next().charAt(0); 
+					scanner.nextLine();
+
+					if (input2 == 'c') {
+
+						Businesses.handlingBusinesses(Businesses.catererNames,Businesses.catererDescriptions,Businesses.catererPrices,Businesses.catererEmails,Businesses.catererPasswords);
+						condition2 = false;
+						condition1 = false;
+
+					}
+
+					else if (input2 == 'v') {
+
+						Businesses.handlingBusinesses(Businesses.venueNames,Businesses.venueDescriptions,Businesses.venuePrices,Businesses.venueEmails,Businesses.venuePasswords);
+						condition2 = false;
+						condition1 = false;
+						
+					}
+
+					else if (input2 == 'b') {
+
+						Businesses.handlingBusinesses(Businesses.bandNames,Businesses.bandDescriptions,Businesses.bandPrices,Businesses.bandEmails,Businesses.bandPasswords);
+						condition2 = false;
+						condition1 = false;
+						
+					}
+
+					else {
+
+						System.out.println("Wrong input");
+
+					}
+
+				}
 			}
 
 			else {
@@ -91,9 +137,9 @@ public class Main {
 
 		System.out.println("Hello. Welcome to Wedinna");
 		System.out.println("");
-		System.out.println("Are you the couple or a guest? ");
+		System.out.println("Are you the couple, a guest or a business? ");
 		System.out.println("");
-		System.out.println("Press c for couple or g for guest:");
+		System.out.println("Press c for couple, g for guest or b for business:");
 
 	}
 
